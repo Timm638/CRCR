bc96e3b0ed38438af0cbdcb7c614387c21c3712f
Lena <lena@fau.de>
2015-02-09 15:38:15 +0100

    ce calendar

--- calendrcreatr.py
+++ calendrcreatr.py
@@ -12,30 +12,30 @@ stammtischwochen = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
 ferienzeiträume = []
 freietage = []
 sondertage = []
-filename = "templates/kalender_prepared.svg"
-
+filename = "templates/kalender_fsce.svg"
+stylefile="templates/style1.css"
 
 #config lesen
 try:
-	conf = open("calendar.conf", "r")
-	configIterator = itertools.filterfalse(lambda l:l[0]=="#" or l[0] == '\n' or len(l)==0, conf)
-	conf.readline =  lambda:next(configIterator)
+    conf = open("calendar_namen.conf", "r")
+    configIterator = itertools.filterfalse(lambda l:l[0]=="#" or l[0] == '\n' or len(l)==0, conf)
+    conf.readline =  lambda:next(configIterator)
 
-	#zeile fuer zeile einlesen, adaequat splitten und zu int konvertieren
-	jahr = int(conf.readline())
-	stammtischwochen = list(map(int, conf.readline().split(",")))
-	ferienzeitraeume = [(tuple(map(int,von.split("."))),tuple(map(int,bis.split(".")))) for von, bis in [zeitraum.split("-") for zeitraum in conf.readline().split(",")]]
-	freietage = [tuple(map(int,b.split("."))) for b in conf.readline().split(",")]
+    #zeile fuer zeile einlesen, adaequat splitten und zu int konvertieren
+    jahr = int(conf.readline())
+    stammtischwochen = list(map(int, conf.readline().split(",")))
+    ferienzeitraeume = [(tuple(map(int,von.split("."))),tuple(map(int,bis.split(".")))) for von, bis in [zeitraum.split("-") for zeitraum in conf.readline().split(",")]]
+    freietage = [tuple(map(int,b.split("."))) for b in conf.readline().split(",")]
 
-	#ab hier nur noch sondertage, bis StopIteration
-	while True:
-		sondertage += [((int(tag),int(monat)), titel, klasse) for (tag,monat,titel,klasse) in [conf.readline().replace(".",",").split(","),]]
+    #ab hier nur noch sondertage, bis StopIteration
+    while True:
+        sondertage += [((int(tag),int(monat)), titel, klasse) for (tag,monat,titel,klasse) in [conf.readline().replace(".",",").split(","),]]
 except StopIteration:
-	pass
+    pass
 except Exception as e:
-	print("Fehler beim Einlesen der config")
-	print(e)
-	sys.exit()
+    print("Fehler beim Einlesen der config")
+    print(e)
+    sys.exit()
 
 
 #einige hilfsstrukturen initialisieren
@@ -45,13 +45,13 @@ wochentagoffset = datetime.date(jahr, 1, 1).weekday()
 
 #im schaltjahr februar +1 Tag
 if ((jahr%4 == 0 and not jahr%100 == 0) or jahr%400 == 0):
-	monatslaenge[1] += 1
+    monatslaenge[1] += 1
 
 
 #dict mit 1 Eintrag pro Tag, mit offset im jahr als key und tuple(titel, klasse) als value
 sondertage2 = dict((i, []) for i in range(1,367))#367: 365 Tage+Schalttag
 for (d,m),titel,klasse in sondertage:
-	sondertage2[sum(monatslaenge[:m-1])+d] += [(titel,klasse)]
+    sondertage2[sum(monatslaenge[:m-1])+d] += [(titel,klasse)]
 sondertage = sondertage2
 
 #einzelne freien tage eintragen
@@ -61,17 +61,17 @@ freietage += [x for ((d1,m1),(d2,m2)) in ferienzeitraeume for x in range(sum(mon
 
 #vorlage oeffnen
 try:
-	image = xml.dom.minidom.parse(filename)
+    image = xml.dom.minidom.parse(filename)
 except IOError:
-	print("Konnte Vorlage nicht öffnen. Dateiname?")
-	sys.exit(1)
-	
+    print("Konnte Vorlage nicht öffnen. Dateiname?")
+    sys.exit(1)
+
 #css eintragen
 try:
-	image.getElementsByTagName("style")[0].firstChild.replaceWholeText(open("templates/mystyle.css").read())
+    image.getElementsByTagName("style")[0].firstChild.replaceWholeText(open(stylefile).read())
 except IOError:
-	print("Konnte CSS nicht oeffnen. Huh?!")
-	sys.exit()
+    print("Konnte CSS nicht oeffnen. Huh?!")
+    sys.exit()
 
 #jahr eintragen
 textElement = [t for t in image.getElementsByTagName("text") if t.getAttribute("id") == "title"]
@@ -79,76 +79,170 @@ textElement[0].firstChild.replaceWholeText("FSI-Kalender {}".format(jahr))
 
 #jedes rect anschauen
 for rect in image.getElementsByTagName("rect"):
-	if "day" in rect.getAttribute("class"):
-		id = rect.getAttribute("id")
-		
-		#tag und monat ausblenden
-		try:
-			month = int(id[0:id.find("-")])
-			day = int(id[id.find("-")+1:])
-			
-			if not 0 < month < 13 or not 0 < day < 32:
-				raise ValueError()
-		except ValueError:
-			print("Warning: Invalid id {}!".format(id))
-			continue
-		
-		#tage, die es im monat nicht gibt, ausblenden
-		if day > monatslaenge[month-1]:
-			rect.addClass("invisible")
-			rect.nextSibling.nextSibling.addClass("invisible")
-			continue
-		
-		tagImJahr = sum(monatslaenge[:month-1])+day
-		wochentag = (tagImJahr + wochentagoffset)%7
-		rect.setAttribute("weekday", wochentage[wochentag-1])
-		
-		#wochennummer einfuegen
-		offset = 0
-		if wochentag == 1:
-			offset = 15 #15=1/2 fontsize
-			rect.parentNode.insertBefore(xml.dom.minidom.parseString("""<text id="{}-{}-woy" class="weekday" style="font-size:30px" x="{}" y="{}">{}</text>"""
-					.format(month,
-						day, 
-						float(rect.getAttribute("x"))+2,
-						float(rect.getAttribute("y"))+float(rect.getAttribute("height"))/2+10-offset,
-						int(tagImJahr/7)+1)).firstChild, rect.nextSibling)
-		
-		#stammtischwochen
-		if wochentag == 2 and int(tagImJahr/7+1) in stammtischwochen:
-			rect.addClass("trunktable")
-		
-		#ferien und feiertage faerben
-		if tagImJahr in freietage:
-			rect.addClass("holiday")
-		
-		if wochentag == 0 or wochentag == 6:
-			rect.addClass("weekend")
-		
-		#besondere tage (geburtstage, ccc) kennzeichnen
-		if tagImJahr in sondertage:
-			#pro eintrag an diesem tag klasse setzen, bezeichnung einfuegen
-			for i, sondertag in enumerate(sondertage[tagImJahr]):
-				rect.addClass(sondertag[1])
-				rect.parentNode.insertBefore(xml.dom.minidom.parseString("""<text id="{}-{}-special" class="specialdaytext" style="font-size:{}px;" x="{}" y="{}">{}</text>"""
-						.format(month,
-							day, 
-							#textgroesse haengt von anzahl der eintraege ab
-							float(rect.getAttribute("height"))/(0.7+0.7*len(sondertage[tagImJahr])),
-							float(rect.getAttribute("x"))+45+(10 if len(sondertage[tagImJahr])>1 else 0),
-							#y mit magischer formel bestimmen, die auch die anzahl der eintraege mit einbezieht
-							float(rect.getAttribute("y"))+float(rect.getAttribute("height"))/len(sondertage[tagImJahr])*(i+1)-0.3*(float(rect.getAttribute("height"))/(0.7+0.7*len(sondertage[tagImJahr]))),
-							sondertag[0])).firstChild, rect.nextSibling)
-			
-		
-		#wochentag einfuegen
-		rect.parentNode.insertBefore(xml.dom.minidom.parseString("""<text id="{}-{}-weekday" style="font-size:30px;" x="{}" y="{}">{}</text>"""
-					.format(month,
-						day, 
-						float(rect.getAttribute("x"))+2,
-						float(rect.getAttribute("y"))+float(rect.getAttribute("height"))/2+10+offset,
-						wochentage[wochentag-1][0:2])).firstChild, rect.nextSibling)
-			
+    if "title bar" in rect.getAttribute("class"):
+        rect.addClass("titlebar")
+
+    if "day" in rect.getAttribute("class"):
+        id = rect.getAttribute("id")
+
+        #tag und monat ausblenden
+        try:
+            month = int(id[0:id.find("-")])
+            day = int(id[id.find("-")+1:])
+
+            if not 0 < month < 13 or not 0 < day < 32:
+                raise ValueError()
+        except ValueError:
+            print("Warning: Invalid id {}!".format(id))
+            continue
+
+        #tage, die es im monat nicht gibt, ausblenden
+        if day > monatslaenge[month-1]:
+            rect.addClass("invisible")
+            rect.nextSibling.nextSibling.addClass("invisible")
+            continue
+
+        tagImJahr = sum(monatslaenge[:month-1])+day
+        wochentag = (tagImJahr + wochentagoffset)%7
+        rect.setAttribute("weekday", wochentage[wochentag-1])
+
+        #wochennummer einfuegen
+        offset = 0
+        if wochentag == 1:
+            offset = 15 #15=1/2 fontsize
+            rect.parentNode.insertBefore(xml.dom.minidom.parseString("""<text id="{}-{}-woy" class="weekday" style="font-size:30px" x="{}" y="{}">{}</text>"""
+                    .format(month,
+                        day,
+                        float(rect.getAttribute("x"))+16,
+                        float(rect.getAttribute("y"))+float(rect.getAttribute("height"))/2+10-offset,
+                        int(tagImJahr/7)+1)).firstChild, rect.nextSibling)
+
+        #stammtischwochen
+        if wochentag == 2 and int(tagImJahr/7+1) in stammtischwochen:
+            rect.addClass("trunktable")
+
+        #ferien und feiertage faerben
+        if tagImJahr in freietage:
+            rect.addClass("holiday")
+
+        if wochentag == 0 or wochentag == 6:
+            rect.addClass("weekend")
+
+        #besondere tage (geburtstage etc.) kennzeichnen
+        if tagImJahr in sondertage:
+            for i, sondertag in enumerate(sondertage[tagImJahr]):
+                rect.addClass(sondertag[1])
+
+
+        if tagImJahr in sondertage:
+            #pro eintrag an diesem tag klasse setzen, bezeichnung einfuegen
+            for i, sondertag in enumerate(sondertage[tagImJahr]):
+                if "birthday" in sondertag[1]:
+                    #rect.addClass(sondertag[1])
+                    rect.parentNode.insertBefore(xml.dom.minidom.parseString("""<text id="{}-{}-special" class="birthdaytext" style="font-size:{}px;" x="{}" y="{}">{}</text>"""
+                           .format(month,
+                                day,
+                                #textgroesse haengt von anzahl der eintraege ab
+                                float(rect.getAttribute("height"))/(1.7+0.5*len(sondertage[tagImJahr])),
+                                float(rect.getAttribute("x"))+60+(10 if len(sondertage[tagImJahr])>1 else 0),
+                                #y mit magischer formel bestimmen, die auch die anzahl der eintraege mit einbezieht
+                                float(rect.getAttribute("y"))+float(rect.getAttribute("height"))/len(sondertage[tagImJahr])*(i+1)-0.3*(float(rect.getAttribute("height"))/(0.7+0.7*len(sondertage[tagImJahr]))),
+                                sondertag[0])).firstChild, rect.nextSibling)
+                else:
+                    if "feiertag" in rect.getAttribute("class"):
+                        rect.addClass("weekend")
+                    #rect.addClass(sondertag[1])
+                    rect.parentNode.insertBefore(xml.dom.minidom.parseString("""<text id="{}-{}-special" class="feiertagstext" style="font-size:{}px;" x="{}" y="{}">{}</text>"""
+                        .format(month,
+                            day,
+                            #textgroesse haengt von anzahl der eintraege ab
+                            float(rect.getAttribute("height"))/(2.5+0.5*len(sondertage[tagImJahr])),
+                            float(rect.getAttribute("x"))+60+(10 if len(sondertage[tagImJahr])>1 else 0),
+                            #y mit magischer formel bestimmen, die auch die anzahl der eintraege mit einbezieht
+                            float(rect.getAttribute("y"))+float(rect.getAttribute("height"))/len(sondertage[tagImJahr])*(i+1)-0.3*(float(rect.getAttribute("height"))/(0.7+0.7*len(sondertage[tagImJahr]))),
+                            sondertag[0])).firstChild, rect.nextSibling)
+
+
+        #wochentag einfuegen
+        rect.parentNode.insertBefore(xml.dom.minidom.parseString("""<text id="{}-{}-weekday" style="font-size:25px;" x="{}" y="{}">{}</text>"""
+                    .format(month,
+                        day,
+                        float(rect.getAttribute("x"))+16,
+                        float(rect.getAttribute("y"))+float(rect.getAttribute("height"))/2+10+offset,
+                        wochentage[wochentag-1][0:2])).firstChild, rect.nextSibling)
+
+
+
+    if "mark" in rect.getAttribute("class"):
+        id = rect.getAttribute("id")
+
+        #tag und monat ausblenden
+        try:
+            month = int(id[0:id.find("-")])
+            day = int(id[id.find("-")+1:])
+
+            if not 0 < month < 13 or not 0 < day < 32:
+                raise ValueError()
+        except ValueError:
+            print("Warning: Invalid id {}!".format(id))
+            continue
+
+        if day > monatslaenge[month-1]:
+            rect.addClass("invisible")
+            continue
+
+        tagImJahr = sum(monatslaenge[:month-1])+day
+
+        #besondere tage (geburtstage, ccc) kennzeichnen
+        if tagImJahr in sondertage:
+            for i, sondertag in enumerate(sondertage[tagImJahr]):
+                rect.addClass(sondertag[1])
+
+
+        if tagImJahr in sondertage:
+            for i, sondertag in enumerate(sondertage[tagImJahr]):
+                if "birthday" in rect.getAttribute("class"):
+                    rect.addClass("birthdaymark")
+                if "berg" in rect.getAttribute("class"):
+                    rect.addClass("bergmark")
+                if "stammtisch" in rect.getAttribute("class"):
+                    rect.addClass("stammtischmark")
+                if "sonstiges" in rect.getAttribute("class"):
+                    rect.addClass("sonstigesmark")
+
+
+        if tagImJahr in freietage:
+            if "birthday" not in rect.getAttribute("class") and "berg" not in rect.getAttribute("class") and "stammtisch" not in rect.getAttribute("class") and "sonstiges" not in rect.getAttribute("class"):
+                rect.addClass("holidaymark")
+
+
+
+
+
+        if "holidaymark" not in rect.getAttribute("class") and "birthday" not in rect.getAttribute("class") and "berg" not in rect.getAttribute("class") and "stammtisch" not in rect.getAttribute("class") and "sonstiges" not in rect.getAttribute("class"):
+            rect.addClass("invisible")
+
+
+    if "frame" in rect.getAttribute("class"):
+        id = rect.getAttribute("id")
+
+        #tag und monat ausblenden
+        try:
+            month = int(id[0:id.find("-")])
+            day = int(id[id.find("-")+1:])
+
+            if not 0 < month < 13 or not 0 < day < 32:
+                raise ValueError()
+        except ValueError:
+            print("Warning: Invalid id {}!".format(id))
+            continue
+
+        if day > monatslaenge[month-1]:
+            rect.addClass("invisible")
+            continue
+
+
+        rect.addClass("nofill");
 
 f = open('outputCalendar.svg','w')
 
